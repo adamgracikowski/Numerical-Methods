4e20918d8c2d7e727b3e330984c4a0dbad0eb04e
Adam <admgrac@gmail.com>
2023-12-24 20:06:28 +0100

    fix randi3diag issue in project2

--- Project2/randi3diag.m
+++ Project2/randi3diag.m
@@ -3,19 +3,23 @@ function T = randi3diag(n, imin, imax)
 % Adam Grącikowski, 327350
 %
 % Generates a tridiagonal, symmetric and real matrix, which main diagonal,
-% as well as supradiagonal and subdiagonal contains random integers from 
+% as well as supradiagonal and subdiagonal contains random integers from
 % the interval [imin, imax].
 % INPUT:
 %   n    - size of the tridiagonal matrix.
-%   imin - left verge of the interval, from which elements of the matrix 
+%   imin - left verge of the interval, from which elements of the matrix
 %          are randomly chosen.
-%   imax - right verge of the interval, from which elements of the matrix 
+%   imax - right verge of the interval, from which elements of the matrix
 %          are randomly chosen.
 % Output:
 %   T - tridiagonal matrix
 
 x = randi([imin, imax], 1, n);
 y = randi([imin, imax], 1, n-1);
+while all(x(1:end-1) == y)
+    x = randi([imin, imax], 1, n);
+    y = randi([imin, imax], 1, n-1);
+end % while
 T = generate3diag(x, y);
 
 end % function
